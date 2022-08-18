b1dd293e6b90771a08f9e07a55dac35a490dea5d
Laura <laurakcleve@gmail.com>
2022-08-18 16:43:10 -0600

    Section 6: create event

---
+++ utils/connectContract.js
@@ -0,0 +1,25 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+  const contractAddress = "0x9905835cc7760146Df034623E0534F8C86Ef1701";
+  const contractABI = abiJSON.abi;
+  let rsvpContract;
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      //checking for eth object in the window
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+  return rsvpContract;
+}
+
+export default connectContract;
